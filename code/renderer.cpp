4bffc8902aff6ac2c414454c4c4656904fb8005d
echelon0 <axvladimirov@outlook.com>
2019-01-29 20:59:55 -0800

    worked on NEE

--- code/renderer.cpp
+++ code/renderer.cpp
@@ -98,7 +98,8 @@ CollisionRoutine(Scene *scene, vec3 ro, vec3 rd) {
     CollisionInfo result;
     
     result.t = FLT_MAX;
-                
+
+    f32 epsilon = 0.001f;
     for(int entityIndex = 0; entityIndex < scene->entities.size; entityIndex++) {
         Entity currentEntity = scene->entities[entityIndex];
         if(currentEntity.isShape) { //--- Shape ---
@@ -112,14 +113,14 @@ CollisionRoutine(Scene *scene, vec3 ro, vec3 rd) {
                     if(discriminant >= 0) {
                         f32 t1 = (-b - (f32)sqrt(discriminant)) / 2.0f * a;
                         f32 t2 = (-b + (f32)sqrt(discriminant)) / 2.0f * a;
-                        if((t1 > 0.0001f) || (t2 > 0.0001f)) {
+                        if((t1 > epsilon) || (t2 > epsilon)) {
                             f32 t_temp = pos_min(t1, t2);
                             if(t_temp < result.t) {
                                 result.t = t_temp;
                                 result.p = ro + result.t*rd;               
                                 result.n = normalize(result.p - currentEntity.offset);
                                 result.entityMat = currentEntity.shape.material;
-                                result.entityIndex = entityIndex;                                      
+                                result.entityIndex = entityIndex;
                             }
                         }
                     }
@@ -129,7 +130,7 @@ CollisionRoutine(Scene *scene, vec3 ro, vec3 rd) {
                 case PLANE: {
                     f32 t_temp = dot((currentEntity.shape.pPlane - ro), currentEntity.shape.nPlane) /
                         dot(rd, currentEntity.shape.nPlane);
-                    if((t_temp > 0.0001f) && (t_temp < result.t) && (dot(currentEntity.shape.nPlane, -rd) > 0.0f)) {
+                    if((t_temp > epsilon) && (t_temp < result.t) && (dot(currentEntity.shape.nPlane, -rd) > 0.0f)) {
                         result.t = t_temp;
                         result.p = ro + result.t*rd;
                         result.n = currentEntity.shape.nPlane;
@@ -152,7 +153,7 @@ CollisionRoutine(Scene *scene, vec3 ro, vec3 rd) {
                 vec3 intersection;                      
                 f32 t_temp;
                 if(ray_intersects_triangle(ro, rd, v0, v1, v2, n, t_temp)) {
-                    if((t_temp > 0.0001f) && (t_temp < result.t) && (dot(n, rd) > 0.0f)) {
+                    if((t_temp > epsilon) && (t_temp < result.t) && (dot(n, rd) > 0.0f)) {
                         result.t = t_temp;
                         result.p = intersection;
                         result.n = n;
@@ -169,6 +170,9 @@ CollisionRoutine(Scene *scene, vec3 ro, vec3 rd) {
 
 f32
 ApproximateUnoccludedArea(Scene *scene, u32 objectIndex, vec3 p, u32 nSamples) {
+    if(nSamples == 0) 
+        return 0;
+    
     Entity object = scene->entities[objectIndex];
     f32 dist = magnitude(object.offset - p);
     vec3 rayToObject = normalize(object.offset - p);    
@@ -189,9 +193,67 @@ ApproximateUnoccludedArea(Scene *scene, u32 objectIndex, vec3 p, u32 nSamples) {
     return (f32)numberOfHits / (f32)nSamples;
 }
 
+
+vec3
+TracePath(Scene *scene, vec3 ro, vec3 rd, u32 pathDepth) {
+    CollisionInfo collision = {};
+    f32 NEE_Attenuation = 1.0f;
+
+    collision = CollisionRoutine(scene, ro, rd);
+    Entity *entity = &scene->entities[collision.entityIndex];
+    
+    if(collision.t == FLT_MAX)
+        return vec3(0.0f, 0.0f, 0.0f);
+    if(entity->isEmitter)
+        return entity->emission.flux * entity->emission.color;
+    if(pathDepth == 0)
+        return vec3(0.0f, 0.0f, 0.0f);
+                
+    //rendering function
+    u32 emitterCount = 0;
+    /*
+    vec3 directLight = vec3(0.0f, 0.0f, 0.0f);
+    for(u32 entityIndex = 0; entityIndex < (u32)scene->entities.size; entityIndex++) {
+        if(scene->entities[entityIndex].isEmitter) {
+            Entity *emitter = &scene->entities[entityIndex];
+                        
+            vec3 BRDF = collision.entityMat.diffuse / (f32)PI;
+            vec3 rayToEmitterCenter = normalize(emitter->offset - collision.p);
+            f32 cos_theta = max(0.0f, dot(collision.n, rayToEmitterCenter));
+
+            f32 distToEmitter = dist(emitter->offset, collision.p);
+            f32 attenuation = 1.0f / max(1.0f, (distToEmitter * distToEmitter));
+            vec3 incomingRadiance = emitter->emission.flux * emitter->emission.color * attenuation;
+
+            if(entityIndex != collision.entityIndex) {
+                u32 nSamples = 0;
+                f32 percentUnoccluded = ApproximateUnoccludedArea(scene, entityIndex, collision.p, nSamples);
+                directLight *= percentUnoccluded;
+            }
+    
+            vec3 emittedRadiance = vec3(0.0f, 0.0f, 0.0f);
+            if(scene->entities[collision.entityIndex].isEmitter) {
+                emittedRadiance = scene->entities[collision.entityIndex].emission.flux * scene->entities[collision.entityIndex].emission.color;
+            }
+            directLight += (emittedRadiance + (BRDF * incomingRadiance * cos_theta));
+            emitterCount++;
+        }
+    }
+    directLight /= (f32)emitterCount;
+    */
+    vec3 BRDF = collision.entityMat.diffuse / (f32)PI;
+    f32 cosTheta = max(0.0f, dot(collision.n, -rd));
+    
+    ro = collision.p;
+    rd = UniformSampleHemisphere(collision.n);
+
+    return BRDF * TracePath(scene, ro, rd, pathDepth - 1) * cosTheta;
+}
+
 void
 Draw(Camera *camera, Scene *scene) {
-    int bounceCount = 8;
+    int pathDepth = 16;
+    
     if(camera->updated || scene->updated) {
         memset(camera->film.sumBuffer, (u8)0, camera->film.pixelWidth * camera->film.pixelHeight * sizeof(u32) * 4);
         camera->sampleCount = 0;
@@ -207,57 +269,7 @@ Draw(Camera *camera, Scene *scene) {
             vec3 ro = camera->pos;
             vec3 rd = normalize(pFilm - camera->pos);
 
-            int bounce = 0;
-            vec3 finalColor = vec3(1.0f, 1.0f, 1.0f);      
-
-            CollisionInfo collision = {};
-            f32 attenuationDist = 1.0f;
-            while(bounce < bounceCount) {
-
-                collision = CollisionRoutine(scene, ro, rd);
-                if(collision.t == FLT_MAX)
-                    break;
-                
-                //rendering function
-                vec3 outgoingRadiance = vec3(0.0f, 0.0f, 0.0f);
-                u32 emitterCount = 0;
-
-                for(u32 entityIndex = 0; entityIndex < (u32)scene->entities.size; entityIndex++) {
-                    if(scene->entities[entityIndex].isEmitter) {
-                        Entity *emitter = &scene->entities[entityIndex];
-                        
-                        vec3 BRDF = collision.entityMat.diffuse / (f32)PI;
-                        vec3 rayToEmitterCenter = normalize(emitter->offset - collision.p);
-                        f32 cos_theta = max(0.0f, dot(collision.n, rayToEmitterCenter));
-
-                        f32 distToEmitter = dist(emitter->offset, collision.p);
-                        f32 attenuation = 1.0f / max(1.0f, (distToEmitter * distToEmitter));
-                        vec3 incomingRadiance = emitter->emission.flux * emitter->emission.color * attenuation;
-                        /*
-                        if(entityIndex != collision.entityIndex) {
-                            u32 nSamples = 5;
-                            f32 percentUnoccluded = ApproximateUnoccludedArea(scene, entityIndex, collision.p, nSamples);
-                            incomingRadiance *= percentUnoccluded;
-                        }
-                        */
-                        vec3 emittedRadiance = vec3(0.0f, 0.0f, 0.0f);
-                        if(scene->entities[collision.entityIndex].isEmitter) {
-                            emittedRadiance = scene->entities[collision.entityIndex].emission.flux * scene->entities[collision.entityIndex].emission.color;
-                        }
-                        outgoingRadiance += (emittedRadiance + (BRDF * incomingRadiance * cos_theta));
-                        emitterCount++;
-                    }
-                }
-                outgoingRadiance /= (f32)emitterCount;
-                finalColor *= outgoingRadiance;
-                
-                vec3 reflection = (dot(-rd, collision.n) * 2.0f * collision.n) + rd;
-                
-                rd = UniformSampleHemisphere(collision.n);
-                ro = collision.p;                
-
-                bounce++;
-            }
+            vec3 finalColor = TracePath(scene, ro, rd, pathDepth);
 
             finalColor /= 1.0f + magnitude(finalColor);     
             finalColor = vec3((f32)pow((double)finalColor.x, (double)(1.0/2.2)), //gamma correction
--- code/sample_scenes.h
+++ code/sample_scenes.h
@@ -11,7 +11,7 @@ DemoScene1(Camera *camera, Scene *scene) {
 
     Entity entity = {};
 
-    vec3 start = vec3(-3.0f, -0.5f, 5.5f);
+    vec3 start = vec3(-3.0f, -0.5f, 6.0f);
     entity.isShape = true;
     entity.shape.type = SPHERE;
     entity.shape.material.diffuse = rgb(255, 0, 0);
@@ -20,7 +20,7 @@ DemoScene1(Camera *camera, Scene *scene) {
     scene->entities.PushBack(entity);
 
     vec3 spacing = vec3((2.0f * absf(start.x) / 3.0f), 0.0f, 0.0f);
-    
+
     entity.shape.material.diffuse = rgb(0, 255, 0);
     entity.offset += spacing;
     scene->entities.PushBack(entity);
@@ -36,21 +36,56 @@ DemoScene1(Camera *camera, Scene *scene) {
     //ground
     entity.isShape = true;
     entity.shape.type = PLANE;
-    entity.shape.material.diffuse = rgb(255, 255, 255);
+    entity.shape.material.diffuse = rgb(180, 180, 180);
     entity.shape.pPlane = vec3(0.0, -1.5f, 0.0);
     entity.shape.nPlane = vec3(0.0f, 1.0f, 0.0f);
     scene->entities.PushBack(entity);
 
+    //left wall
+    entity.shape.material.diffuse = rgb(198, 192, 135);
+    entity.shape.pPlane = vec3(-4.0, 0.0f, 0.0);
+    entity.shape.nPlane = vec3(1.0f, 0.0f, 0.0f);
+    scene->entities.PushBack(entity);
+
+    //right wall
+    entity.shape.material.diffuse = rgb(198, 192, 135);
+    entity.shape.pPlane = vec3(4.0, 0.0f, 0.0);
+    entity.shape.nPlane = vec3(-1.0f, 0.0f, 0.0f);
+    scene->entities.PushBack(entity);
+
+    //front wall
+    entity.shape.material.diffuse = rgb(255, 255, 255);
+    entity.shape.pPlane = vec3(0.0, 0.0f, 8.0);
+    entity.shape.nPlane = vec3(0.0f, 0.0f, -1.0f);
+    scene->entities.PushBack(entity);
+
+    //back wall
+    entity.shape.material.diffuse = rgb(180, 180, 180);
+    entity.shape.pPlane = vec3(0.0, 0.0f, -2.0);
+    entity.shape.nPlane = vec3(0.0f, 0.0f, 1.0f);
+    scene->entities.PushBack(entity);
+
+    //top wall
+    entity.shape.material.diffuse = rgb(255, 255, 255);
+    entity.isEmitter = true;    
+    entity.shape.pPlane = vec3(0.0, 5.0f, 0.0);
+    entity.emission.flux = 1000.0f;
+    entity.emission.color = rgb(255, 255, 255);
+    entity.shape.nPlane = vec3(0.0f, 1.0f, 0.0f);
+    scene->entities.PushBack(entity);    
+
     //light
+    /*
     Entity light = {};
     light.isShape = true;
     light.shape.type = SPHERE;    
     light.isEmitter = true;    
-    light.shape.radius = 0.5f;
-    light.emission.flux = 100.0f;
+    light.shape.radius = 3.0f;
+    light.emission.flux = 1000.0f;
     light.emission.color = rgb(255, 255, 255);
-    light.offset = vec3(0.0f, 3.0f, 0.0f);
+    light.offset = vec3(0.0f, 3.0f, 0.0f);    
     scene->entities.PushBack(light);
+    */
 }
 
 void 
